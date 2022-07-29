6a027d6654176dc24d616137c068e1edcb3ab1f1
thiago <thiagooffice2@gmail.com>
2022-07-28 22:42:15 -0300

    feat: creating first route test

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
 
-const port = 5000
+const port = process.env.PORT
 
 const app = express()
 
 // config JSON and form data response
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// routes
+const router = require("./routes/Router.js")
+
+app.use(router)
 
 app.listen(port, ()=>{
     console.log(`App rodando na porta ${port}`);
