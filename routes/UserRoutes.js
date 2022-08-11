4d0c69fea6005292d915322b95d1696df2463a9b
Thiago <thiagooffice2@gmail.com>
2022-08-10 23:00:42 -0300

    feat: creating first middleware

---
+++ routes/UserRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+//Controller
+const {register} = require("../controllers/UserController")
+
+//Middlewares
+const validate = require("../middlewares/handleValidation")
+
+//Routes
+router.post("/register", validate, register)
+module.exports = router
