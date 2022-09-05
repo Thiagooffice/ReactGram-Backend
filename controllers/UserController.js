efa8959b3a823acc7d1cb695c390f401d556a899
Thiago <thiagooffice2@gmail.com>
2022-09-05 12:22:26 -0300

    feat: login validation

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,8 +13,47 @@ const generateToken = (id) => {
 }
 //Register user and sign in
 const register = async(req, res) => {
-    res.send("Registro")
+    
+    const {name, email, password}  =req.body
+
+    //check if user exists
+    const user = await User.findOne({email})
+
+    if(user){
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail."]})
+        return
+    }
+
+    //Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user 
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    //If user was created sucessfully, return the token
+    if(!newUser){
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+
 }
+
+//Sign user in
+const login = (req, res) => {
+    res.send("Login")
+}
+
 module.exports = {
     register,
+    login
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,12 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 //Controller
-const {register} = require("../controllers/UserController")
+const {register, login} = require("../controllers/UserController")
 
 //Middlewares
 const validate = require("../middlewares/handleValidation")
-const { useCreateValidation, userCreateValidation } = require("../middlewares/useValidations")
+const { userCreateValidation,  loginValidation } = require("../middlewares/useValidations")
 
 //Routes
 router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
+
 module.exports = router
