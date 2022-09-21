41c11e7717d737c03ca7612b6e4d297c2d4348fb
Thiago <thiagooffice2@gmail.com>
2022-09-21 17:30:29 -0300

    feat: taking all the photos

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -63,7 +63,17 @@ const deletePhoto = async (req, res) => {
   }
 };
 
+//get all photos
+const getAllPhotos = async(req, res) => {
+
+    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec()
+
+    return res.status(200).json(photos)
+
+}
+
 module.exports = {
   insertPhoto,
   deletePhoto,
+  getAllPhotos
 };
