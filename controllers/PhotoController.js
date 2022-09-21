88e4943f0cc8abb03aeb4b805f0a19c299192831
Thiago <thiagooffice2@gmail.com>
2022-09-21 18:26:56 -0300

    feat: update a photo

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -84,25 +84,66 @@ const getUserPhotos = async (req, res) => {
 
 //get photo by id
 const getPhotoById = async (req, res) => {
+  const { id } = req.params;
 
-    const {id} = req.params
+  const photo = await Photo.findById(mongoose.Types.ObjectId(id));
 
-    const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+  //check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada."] });
+    return;
+  }
 
-    //check if photo exists
-    if(!photo){
-        res.status(404).json({errors: ["Foto não encontrada."]})
-        return
-    }
+  res.status(200).json(photo);
+};
 
-    res.status(200).json(photo)
+// Update a photo
+const updatePhoto = async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-}
+  let image;
+
+  if (req.file) {
+    image = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const photo = await Photo.findById(id);
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
+
+  // Check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res
+      .status(422)
+      .json({ errors: ["Ocorreu um erro, tente novamente mais tarde"] });
+    return;
+  }
+
+  if (title) {
+    photo.title = title;
+  }
+
+  if (image) {
+    photo.image = image;
+  }
+
+  await photo.save();
+
+  res.status(200).json({ photo, message: "Foto atualizada com sucesso!" });
+};
 
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
-  getPhotoById
+  getPhotoById,
+  updatePhoto,
 };
